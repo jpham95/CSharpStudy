54468922c166efda1b2deb0f5778a190ee76acd9
Jacob Pham <jacobpham95@gmail.com>
2023-11-15 16:14:02 +1100

    VSCode testing working

---
+++ Program/Tests/test_bst.cs
@@ -0,0 +1,150 @@
+using NUnit.Framework;
+using System;
+//using DataStructures;
+
+namespace DataStructures.Tests
+{
+    [TestFixture]
+    public class BinarySearchTreeTests
+    {
+        [Test]
+        public void IsEmpty_NewTree_ReturnsTrue()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+
+            // Act
+            var result = bst.IsEmpty;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Count_NewTree_ReturnsZero()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+
+            // Act
+            var result = bst.Count;
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Insert_NewTree_InsertsValue()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+
+            // Act
+            bst.Insert(5);
+
+            // Assert
+            Assert.IsFalse(bst.IsEmpty);
+            Assert.AreEqual(1, bst.Count);
+            Assert.IsTrue(bst.Contains(5));
+        }
+
+        [Test]
+        public void Insert_DuplicateValue_ThrowsException()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(5);
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => bst.Insert(5));
+        }
+
+        [Test]
+        public void Remove_ValueInTree_RemovesValue()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            // Act
+            bst.Remove(3);
+
+            // Assert
+            Assert.AreEqual(2, bst.Count);
+            Assert.IsFalse(bst.Contains(3));
+        }
+
+        [Test]
+        public void Remove_ValueNotInTree_ThrowsException()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(5);
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => bst.Remove(3));
+        }
+
+        [Test]
+        public void PreorderTraversal_TraversesTreeInPreorder()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            var expected = new int[] { 5, 3, 7 };
+            var actual = new int[3];
+            var index = 0;
+
+            // Act
+            bst.PreorderTraversal(x => actual[index++] = x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void PostorderTraversal_TraversesTreeInPostorder()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            var expected = new int[] { 3, 7, 5 };
+            var actual = new int[3];
+            var index = 0;
+
+            // Act
+            bst.PostorderTraversal(x => actual[index++] = x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InorderTraversal_TraversesTreeInInorder()
+        {
+            // Arrange
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            var expected = new int[] { 3, 5, 7 };
+            var actual = new int[3];
+            var index = 0;
+
+            // Act
+            bst.InorderTraversal(x => actual[index++] = x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Tests/test_bst.cs
+++ Tests/test_bst.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using DataStructures;
 
 namespace DataStructures.Tests
 {
